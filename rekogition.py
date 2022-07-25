956922bcc43828e5dfac56ee4db1ce898d7cdbf0
Mike <63439730+astraeus-sirius@users.noreply.github.com>
2022-07-25 17:36:37 -0400

    Update rekogition.py

--- rekogition.py
+++ rekogition.py
@@ -8,6 +8,7 @@ min_confidence = 80
 max_labels = 10
 
 def list_objects(bucket,region):
+    boto3.setup_default_session(profile_name = profile_name)
     client=boto3.client('s3', region_name=region)
     response = client.list_objects_v2(
         Bucket=bucket
@@ -17,8 +18,8 @@ def list_objects(bucket,region):
         object_list.append(object['Key'])
     return(object_list)
 
-
 def detect_labels(photo, bucket):
+    boto3.setup_default_session(profile_name=profile_name)
     client=boto3.client('rekognition', region_name = region)
     response=client.detect_labels(
         Image = {
@@ -28,17 +29,15 @@ def detect_labels(photo, bucket):
             'Name': photo,
             }
         },
-		MaxLabels = max_labels,
-		MinConfidence = min_confidence,
+        MaxLabels = max_labels,
+        MinConfidence = min_confidence,
         )
-    print ("###################"  + " Starting Labels for "  + photo + "###################")
     print()
+    df = pd.DataFrame(response['Labels'])
+    df.to_csv('results.csv', mode='a', index=True, header=True)
     for label in response['Labels']:
-        df = pd.DataFrame(data=response['Labels'])
-        df.columns =['Labels', 'Confidence', 'Instances','Parent']
-        df.to_csv('results.csv', index=True, header=True)
         print ("Label: " + label['Name'])
-        print ("Confidence: " + str(label['Confidence']))
+        print ("Confidence: " + str(label['Confidence']) + "%")
         print ("Instances:")
         for instance in label['Instances']:
             print ("    Bounding box")
@@ -46,18 +45,22 @@ def detect_labels(photo, bucket):
             print ("       Left: " + str(instance['BoundingBox']['Left']))
             print ("       Width: " +  str(instance['BoundingBox']['Width']))
             print ("       Height: " +  str(instance['BoundingBox']['Height']))
-            print ("  Confidence: " + str(instance['Confidence']))
+            print ("  Confidence: " + str(instance['Confidence']) +"%")
             print()
         print ("Parents:")
         for parent in label['Parents']:
             print ("     " + parent['Name'])
+    print ()
+    return response['Labels']
+
+def main():
+    photos = list_objects(bucket, region)
+    for photo in photos:
+        print ()
+        print ("###################"  + " Starting Labels for "  + photo + "###################")
+        detect_labels(photo, bucket)
         print ("###################"  + " Ending Labels for "  + photo + "###################")
         print ()
-    return len(response['Labels'])
 
-
-photos = list_objects(bucket, region)
-
-for photo in photos:
-    detect_labels(photo, bucket)
-
+if __name__ == "__main__":
+    main()
